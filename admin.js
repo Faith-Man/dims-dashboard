14908083d9c49d2d5e1ea54d65d845114b35286a
Faith-Man <31673334+Faith-Man@users.noreply.github.com>
2025-08-31 08:13:42 -0400

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,8 +1,8 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
 // TODO: set your current project values:
-const SUPABASE_URL = 'https://YOUR-REF.supabase.co';
-const SUPABASE_ANON_KEY = 'YOUR-ANON-KEY';
+const SUPABASE_URL = 'https://tfbfkitdenxgcxkxhydw.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRmYmZraXRkZW54Z2N4a3hoeWR3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY2MjY3NDcsImV4cCI6MjA3MjIwMjc0N30.8EtOV90zyP2bn3ZrP35vOwnZkRo4nc72uD1xa964hC4';
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
